5a9b015247df5b7adfca7974a299f1c157610736
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-03-27 13:11:52 -0300

    adjusting train parameters; removing quick test in local image

--- train.py
+++ train.py
@@ -17,17 +17,8 @@ def main():
     # Train the model
     print("Training YOLOv11 model with the training set...")
     data_path = os.path.join(dataset_subfolder, "data.yaml")
-    results = yolo.train(data=data_path, epochs=100,
-                         batch=16, device="cpu", optimizer="adam", patience=0, lrf=0.001)
-
-    # Try in a test image
-    print("Predicting a test image with the trained model...")
-    test_folder = os.path.joint(dataset_subfolder, "test", "images")
-    test_image = os.path.join(
-        test_folder, "snp0206250246_png.rf.5ddee07dffd8452d04487e6029a61d34.jpg")
-    yolo.predict(source=test_image, show=True, save=True, conf=0.70,
-                 line_width=1, save_crop=False, save_txt=False,
-                 show_labels=True, show_conf=True)
+    results = yolo.train(data=data_path, epochs=1000,
+                         batch=16, device="cpu", optimizer="adam", patience=0, lrf=0.0001)
 
 
 if __name__ == "__main__":
