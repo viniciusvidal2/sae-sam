fc9b294a9c5cb2180c00297bbd77270bb5174851
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-04-01 15:19:45 -0300

    updating train params

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ def main():
     print("Training YOLOv11 model with the training set...")
     data_path = os.path.join(dataset_subfolder, "data.yaml")
     results = yolo.train(data=data_path, epochs=2000,
-                         batch=16, device="cpu", optimizer="adam", patience=0, lrf=0.0001)
+                         batch=32, device=0, optimizer="adam", patience=0, lrf=0.0001)
 
 
 if __name__ == "__main__":
